eadee42b0160b06c29796e33b84bfffb120284b8
koichiro59 <114040014+koichiro59@users.noreply.github.com>
2024-07-28 01:48:57 +0900

    Create services.php

---
+++ services.php
@@ -0,0 +1,33 @@
+<?php 
+    include 'header.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="service">
+        <h1>FitLife Workouts</h1>
+        <img src="./assets/images/facility.jpg" alt="">
+        <h2>パーソナライズされたワークアウトプランを提供</h2>
+    </div>
+    <div class="service">
+        <h1>FitLife Eats</h1>
+        <img src="./assets/images/facility.jpg" alt="">
+        <h2>パーソナライズされた栄養プランと食事管理を提供</h2>
+    </div>
+    <div class="service">
+        <h1>FitLife Challenges</h1>
+        <img src="./assets/images/facility.jpg" alt="">
+        <h2>ユーザーがモチベーションを維持し、目標を達成するためのチャレンジやコンペティションを提供</h2>
+    </div>
+</body>
+</html>
+
+<?php   
+    include 'footer.php'; 
+?>

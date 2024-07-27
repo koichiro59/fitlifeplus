0bd36561b7307fcf57b651d67ef4de27cc8f5d3e
koichiro59 <114040014+koichiro59@users.noreply.github.com>
2024-07-28 01:44:48 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="./assets/css/header.css?<?php echo date('Ymd-Hi'); ?>">
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="./assets/images/logo.jpg" alt="ロゴ">
+        </div>
+        <nav class="nav-menu">
+            <a href="./index.php">ホーム</a>
+            <a href="./services.php">事業内容</a>
+            <a href="./about.php">会社概要</a>
+            <a href="./contact.php">お問い合わせ</a>
+        </nav>
+    </header>
+    <div class="blank"></div>
+</body>
+</html>

e00113879741c070cd2e8ac27a82e16ba0f0c001
koichiro59 <114040014+koichiro59@users.noreply.github.com>
2024-07-28 01:45:45 +0900

    Create about.php

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php 
+    include 'header.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="./assets/images/company.jpg" alt="">
+    <h1>会社概要</h1>
+    <table>
+        <tr>
+            <th>会社名</th>
+            <td>FitLifePlus</td>
+        </tr>
+        <tr>
+            <th>設立年</th>
+            <td>2024年</td>
+        </tr>
+        <tr>
+            <th>代表者</th>
+            <td>山田太郎</td>
+        </tr>
+        <tr>
+            <th>電話番号</th>
+            <td>03-1234-5678</td>
+        </tr>
+        <tr>
+            <th>メールアドレス</th>
+            <td>info@fitlifeplus.com</td>
+        </tr>
+    </table>
+    <div>
+        <h1>所在地</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51871.681823163024!2d139.70416676642606!3d35.652861579272695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbbb41922f3%3A0x33ebfd75f2f06cac!2z5p2x5Lqs6YO95riv5Yy6!5e0!3m2!1sja!2sjp!4v1721569581529!5m2!1sja!2sjp" 
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+</body>
+</html>
+
+<?php   
+    include 'footer.php'; 
+?>
